866c63f9c3c5521e85730a50b8ecfd8c6f85df8b
Ervin Chow <ervinchow@gmail.com>
2016-05-19 12:48:58 -0700

    adds unit tests for immutable render behavior, dependences and imports

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -5,6 +5,7 @@ import {
 	scryRenderedDOMComponentsWithTag,
 	Simulate
 } from 'react-addons-test-utils'
+import {List} from 'immutable'
 import Voting from '../../src/components/Voting'
 import {expect} from 'chai'
 
@@ -66,5 +67,25 @@ describe('Voting', () => {
 		const winner = ReactDOM.findDOMNode(component.refs.winner)
 		expect(winner).to.be.ok
 		expect(winner.textContent).to.contain('Trainspotting')
+	})
+
+	it('renders a pure component', () => {
+		const pair = ['Trainspotting', '28 Days Later']
+		const container = document.createElement('div')
+		let component = ReactDOM.render(
+			<Voting pair={pair} />,
+			container
+		)
+
+		let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+		expect(firstButton.textContent).to.equal('Trainspotting')
+
+		pair[0] = 'Sunshine'
+		component = ReactDOM.render(
+			<Voting pair={pair} />,
+			container
+		)
+		firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+		expect(firstButton.textContent).to.equal('Trainspotting')
 	})
 })
