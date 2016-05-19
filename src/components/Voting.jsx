7890f0d41b4098f71ab4457389670b8ddf51fa5f
Ervin Chow <ervinchow@gmail.com>
2016-05-18 20:29:26 -0700

    adds conditional rendering based on props

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,12 +4,25 @@ export default React.createClass({
 	getPair: function() {
 		return this.props.pair || []
 	},
+	isDisabled: function(){
+		return !!this.props.hasVoted
+	},
+	hasVotedFor: function(entry){
+		return this.props.hasVote === entry
+	},
 	render: function() {
 		return (
 			<div className='voting'>
-				{this.getPair().map(entry => 
-					<button key={entry}>
+				{this.props.winner ?
+					<div ref="winner">Winner is {this.props.winner}!</div> :
+					this.getPair().map(entry => 
+					<button key={entry}
+									disabled={this.isDisabled()}
+									onClick={() => this.props.vote(entry)}>
 						<h1>{entry}</h1>
+						{this.hasVotedFor(entry) ? 
+							<div className="label">Voted</div> :
+							null}
 					</button>
 				)}
 			</div>
