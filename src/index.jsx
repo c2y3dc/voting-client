85ca67c43ed6de95132dc1e347fc194e38149d93
Ervin Chow <ervinchow@gmail.com>
2016-05-25 14:22:26 -0700

    adds react-redux provider to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
-import reducer from './reducer'
 import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducer'
 import App from './components/App'
-import Voting from './components/Voting'
+import {VotingContainer} from './components/Voting'
 import Results from './components/Results'
 
 const store = createStore(reducer)
@@ -21,11 +22,13 @@ store.dispatch({
 const routes = (
 	<Route component={App}>
 		<Route path="/results" component={Results} />
-		<Route path="/" component={Voting} />
+		<Route path="/" component={VotingContainer} />
 	</Route>
 )
 
 ReactDOM.render(
-	<Router history={hashHistory}>{routes}</Router>,
+	<Provider>
+		<Router history={hashHistory}>{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 )
