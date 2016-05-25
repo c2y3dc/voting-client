afd7bf07aa028c21ba1e5eb167d675e42ad4c57a
Ervin Chow <ervinchow@gmail.com>
2016-05-24 17:32:44 -0700

    adds store and dispatches mock action

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
+import reducer from './reducer'
+import {createStore} from 'redux'
 import App from './components/App'
 import Voting from './components/Voting'
 import Results from './components/Results'
 
-const pair = ['Trainspotting', '28 Days Later']
+const store = createStore(reducer)
+store.dispatch({
+	type: 'SET_STATE',
+	state: {
+		vote: {
+			pair: ['Sunshine', '28 Days Later'],
+			tally: {Sunshine: 2}
+		}
+	}
+})
 
 const routes = (
 	<Route component={App}>
