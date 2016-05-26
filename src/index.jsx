59823b5e227974ec37f78c0dae61c37291867fd0
Ervin Chow <ervinchow@gmail.com>
2016-05-25 19:00:13 -0700

    sets up client socket connection

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import io from 'socket.io-client'
 import reducer from './reducer'
 import App from './components/App'
 import {VotingContainer} from './components/Voting'
@@ -18,6 +19,8 @@ store.dispatch({
 		}
 	}
 })
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
 
 const routes = (
 	<Route component={App}>
