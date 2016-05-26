3c292e44d65ba9c194e4f2ce9e9b7b66d2bba55d
Ervin Chow <ervinchow@gmail.com>
2016-05-25 18:31:01 -0700

    connects Results component with redux store, exports pure and connected components and fixes test

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {connect} from 'react-redux'
 import Winner from './Winner'
 import Tally from './Tally'
 
-export default React.createClass({
+export const Results =  React.createClass({
 	mixins: [PureRenderMixin],
 	getPair: function(){
 		return this.props.pair || []
@@ -22,3 +23,13 @@ export default React.createClass({
 		)
 	}
 })
+
+function mapStateToProps(state) {
+	return {
+		pair: state.getIn(['vote', 'pair']),
+		tally: state.getIn(['vote', 'tally']),
+		winner: state.get('winner')
+	}
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results)
