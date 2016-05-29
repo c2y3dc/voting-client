4e93990295dd30f095b53dd550da48f3e7c8e5ed
Ervin Chow <ervinchow@gmail.com>
2016-05-28 18:24:13 -0700

    adds action creators and connects actions with redux

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,6 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 import {connect} from 'react-redux'
 import Winner from './Winner'
 import Vote from './Vote'
+import * as actionCreators from '../action_creators'
 
 export const Voting = React.createClass({
 	mixins: [PureRenderMixin],
@@ -18,11 +19,15 @@ export const Voting = React.createClass({
 function mapStateToProps(state){
 	return {
 		pair: state.getIn(['vote', 'pair']),
+		hasVoted: state.get('hasVoted'),
 		winner: state.get('winner')
 	}
 }
 
-export const VotingContainer = connect(mapStateToProps)(Voting)
+export const VotingContainer = connect(
+	mapStateToProps,
+	actionCreators
+)(Voting)
 
 // export default function Voting(props){
 // 	const entries = props.pair || []
