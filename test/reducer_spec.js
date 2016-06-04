ecd8ef7ae1be673347658cfb2d5a6524696de802
Ervin Chow <ervinchow@gmail.com>
2016-06-04 16:40:25 -0700

    adds improved implementation of vote reset state

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -66,9 +66,10 @@ describe('reducer', () => {
 		}))
 	})
 
-	it('handles VOTE by setting hasVoted', () => {
+	it('handles VOTE by setting myVote', () => {
 		const state = fromJS({
 			vote: {
+				round: 42,
 				pair: ['Trainspotting', '28 Days Later'],
 				tally: {Trainspotting: 1}
 			}
@@ -78,16 +79,21 @@ describe('reducer', () => {
 
 		expect(nextState).to.equal(fromJS({
 			vote: {
+				round: 42,
 				pair: ['Trainspotting', '28 Days Later'],
 				tally: {Trainspotting: 1}
 			},
-			hasVoted: 'Trainspotting'
+			myVote: {
+				round: 42,
+				entry: 'Trainspotting'
+			}
 		}))
 	})
 
-	it('does note set hasVoted for VOTE on invalid entry', () => {
+	it('does note set myVote for VOTE on invalid entry', () => {
 		const state = fromJS({
 			vote: {
+				round: 42,
 				pair: ['Trainspotting', '28 Days Later'],
 				tally: {Trainspotting: 1}
 			}
@@ -97,25 +103,31 @@ describe('reducer', () => {
 
 		expect(nextState).to.equal(fromJS({
 			vote: {
+				round: 42,
 				pair:['Trainspotting', '28 Days Later'],
 				tally: {Trainspotting: 1}
 			}
 		}))
 	})
 
-	it('removes hasVoted on SET_STATE if pair changes', () => {
+	it('removes myVote on SET_STATE if round has changed', () => {
 		const initialState = fromJS({
 			vote: {
+				round: 42,
 				pair: ['Trainspotting', '28 Days Later'],
 				tally:{Trainspotting: 1}
 			},
-			hasVoted: 'Trainspotting'
+			myVote: {
+				round: 42,
+				entry: 'Trainspotting'
+			}
 		})
 		const action = {
 			type: 'SET_STATE',
 			state: {
 				vote: {
-					pair: ['Sunshine', 'Slumdog Millionaire']
+					round: 43,
+					pair: ['Sunshine', 'Trainspotting']
 				}
 			}
 		}
@@ -123,7 +135,8 @@ describe('reducer', () => {
 
 		expect(nextState).to.equal(fromJS({
 			vote: {
-				pair: ['Sunshine', 'Slumdog Millionaire']
+				round: 43,
+				pair: ['Sunshine', 'Trainspotting']
 			}
 		}))
 	})
