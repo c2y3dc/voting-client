c5c68bff28db6d4b99d71658ee5b317b880a8b66
Ervin Chow <ervinchow@gmail.com>
2016-06-06 23:27:14 -0700

    adds styles to vote and result components

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,11 @@ module.exports = {
 			test: /\.jsx?$/,
 			exclude: /node_modules/,
 			loader: 'react-hot!babel'
-		}]
-	},
+		},{
+      test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+    }]
+  },
 	resolve: {
 		extensions: ['','.js', '.jsx']
 	},
